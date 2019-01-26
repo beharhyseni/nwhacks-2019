7c64876df99ee3cd8537ba54d9d6f7449626706a
Behar Hyseni <behar.hyseni@alumni.ubc.ca>
2019-01-26 14:59:41 -0800

    initial project set-up

---
+++ nwhacks2019/app/src/main/java/com/example/nwhacks2019/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.nwhacks2019;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+//    private void dispatchTakePictureIntent() {
+//        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+//        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+//            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+//        }
+//    }
+}
