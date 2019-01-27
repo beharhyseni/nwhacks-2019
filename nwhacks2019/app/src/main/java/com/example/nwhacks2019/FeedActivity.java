7065d9506da73c0d46f58b9f875a260eade720c5
Behar Hyseni <behar.hyseni@alumni.ubc.ca>
2019-01-27 11:47:34 -0800

    implemented scroll view

--- nwhacks2019/app/src/main/java/com/example/nwhacks2019/FeedActivity.java
+++ nwhacks2019/app/src/main/java/com/example/nwhacks2019/FeedActivity.java
@@ -1,5 +1,6 @@
 package com.example.nwhacks2019;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
@@ -11,10 +12,14 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+<<<<<<< HEAD
 import android.view.ViewGroup;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
+=======
+import org.json.JSONObject;
+>>>>>>> 051069b633ab7ccb3d2c75a3c0a2a4d045a42bd5
 
 import org.json.JSONObject;
 
@@ -23,10 +28,24 @@ public class FeedActivity extends AppCompatActivity implements ActionBar.TabList
     private static ActionBar actionBar;
     private static FragmentManager fragmentManager;//fragment manager to work on fragments
 
+    public JSONObject CallServer() {
+        JSONObject results = new JSONObject();
+        try {
+            ScrapeTask task = new ScrapeTask();
+            task.execute();
+            results = task.get();  //Add this
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return results;//Need to get back the result
+
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feed);
+<<<<<<< HEAD
         init();
         setTabs();
     }
@@ -134,6 +153,32 @@ public class FeedActivity extends AppCompatActivity implements ActionBar.TabList
             // Returning no. of counts of fragments
             return 2;
         }
+=======
+        mRecyclerView = findViewById(R.id.my_recycler_view);
+        mRecyclerView.setHasFixedSize(true);
+
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mRecyclerView.setAdapter(mAdapter);
+
+        JSONObject results = CallServer();
+        mAdapter = new PostsAdapter(results, this);
+>>>>>>> 051069b633ab7ccb3d2c75a3c0a2a4d045a42bd5
     }
 
+}
+
+class ScrapeTask extends AsyncTask<Void, Void, JSONObject> {
+
+    @Override
+    protected JSONObject doInBackground(Void... params) {
+        JSONObject results = new JSONObject();
+        try {
+            Scraper scraper = new Scraper();
+            results = scraper.runScraper("lysol wipes", 3);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return results;
+    }
 }
