3246d5b4e0e92e1f8a62e4868fcf74cd42dac442
Behar Hyseni <behar.hyseni@alumni.ubc.ca>
2019-01-26 23:10:03 -0800

    replace azure with google cloud, implemented image text extraction

--- nwhacks2019/app/src/main/java/com/example/nwhacks2019/MainActivity.java
+++ nwhacks2019/app/src/main/java/com/example/nwhacks2019/MainActivity.java
@@ -2,58 +2,100 @@ package com.example.nwhacks2019;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.util.SparseArray;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import edmt.dev.edmtdevcognitivevision.VisionServiceClient;
-import edmt.dev.edmtdevcognitivevision.VisionServiceRestClient;
+import com.google.android.gms.vision.Frame;
+import com.google.android.gms.vision.text.TextBlock;
+import com.google.android.gms.vision.text.TextRecognizer;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+
 
 public class MainActivity extends AppCompatActivity {
 
-ImageView imageView;
+    ImageView imageView;
+    Button btnCamera;
+    Button btnAnalyze;
+    TextView txtResult;
 
-private final String API_KEY = "afb4814ea5374242bad2d032bced892c";
-private final String API_LINK = "https://canadacentral.api.cognitive.microsoft.com/";
+    private final String API_KEY = "2a7ac6f5e6364cd089251fc0b60b6f11";
+    private final String API_LINK = "https://canadacentral.api.cognitive.microsoft.com";
+    Bitmap bitmap;
+    // Vision Client
 
-// Vision Client
-    VisionServiceClient visionServiceClient = new VisionServiceRestClient(API_KEY, API_LINK);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button btnCamera = (Button) findViewById(R.id.btnCamera);
+        btnCamera = (Button) findViewById(R.id.btnCamera);
+        btnAnalyze = (Button) findViewById(R.id.btnAnalyze);
+        txtResult = (TextView) findViewById(R.id.txt_result);
         imageView = (ImageView) findViewById(R.id.imageView);
 
         btnCamera.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(intent,0);
-
+                startActivityForResult(intent, 0);
             }
+
+
+
         });
 
+        btnAnalyze.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                getTextFromImage(v);
+            }
+        });
     }
 
 
+        @Override
+        protected void onActivityResult(int requestCode, int resultCode, Intent data){
+            super.onActivityResult(requestCode, resultCode, data);
+            bitmap = (Bitmap) data.getExtras().get("data");
+            imageView.setImageBitmap(bitmap);
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream);
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data){
-        super.onActivityResult(requestCode, resultCode, data);
-        Bitmap bitmap = (Bitmap)data.getExtras().get("data");
-        imageView.setImageBitmap(bitmap);
+            ByteArrayInputStream inputStream = new ByteArrayInputStream(outputStream.toByteArray());
+
+
+        }
+
+        public void getTextFromImage(View v){
+        TextRecognizer textRecognizer = new TextRecognizer.Builder(getApplicationContext()).build();
+        if(!textRecognizer.isOperational()){
+            Toast.makeText(getApplicationContext(), "Text cannot be recognized!", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            Frame frame = new Frame.Builder().setBitmap(bitmap).build();
+            SparseArray<TextBlock> items = textRecognizer.detect(frame);
+
+            StringBuilder sb = new StringBuilder();
+
+            for(int i =0;i<items.size();++i){
+            TextBlock myItem = items.valueAt(i);
+            sb.append(myItem.getValue());
+            sb.append("\n");
+            }
+
+            txtResult.setText(sb.toString());
+        }
+
+        }
     }
-
-
-//    private void dispatchTakePictureIntent() {
-//        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-//        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-//            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-//        }
-//    }
-}
